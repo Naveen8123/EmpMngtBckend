cb8661023506559485011e2978df2adce2800da3
Naveen <naveennandhi143@gmail.com>
2024-12-11 11:06:34 +0530

    Initial commit

--- src/main/java/com/_TechSoft/EmployeeManagement/entity/Employee.java
+++ src/main/java/com/_TechSoft/EmployeeManagement/entity/Employee.java
@@ -1,6 +1,9 @@
 package com._TechSoft.EmployeeManagement.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +18,19 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+
     @Column(name = "email_id", nullable = false, unique = true)
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
 }
 
